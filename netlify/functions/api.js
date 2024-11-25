8e5996cfab0e3fd14f8c83b0fec3433f3d7b56ce
jsillero-ga <jsillerosalado@gmail.com>
2024-11-25 22:16:16 +0100

    commit

--- netlify/functions/api.js
+++ netlify/functions/api.js
@@ -73,8 +73,6 @@ const startServers = async () => {
         await mongoose.connect(process.env.MONGODB_URI);
         console.log("Database connection established");
 
- 
-
     } catch (error) {
         console.log(error);
     }
