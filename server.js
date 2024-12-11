6bdc6010fd4f404643abb9b6bd5a1a6390471062
jsillero-ga <jsillerosalado@gmail.com>
2024-12-11 19:11:09 +0100

    changes

--- server.js
+++ server.js
@@ -28,7 +28,8 @@ app.use(express.urlencoded({ extended: false }))
 app.use(morgan('dev'));
 app.use(flash());
 // Set direction for styles
-app.use(express.static(path.join(__dirname, 'views')));
+//app.use(express.static(path.join(__dirname, 'views')));
+app.use(express.static("public"));
 
 
 //session manager
